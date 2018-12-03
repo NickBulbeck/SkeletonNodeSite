46e01a3af6a138685207f5efe18c61ebb73d2923
NickBulbeck <bulbecknick@gmail.com>
2018-12-03 16:20:14 +0000

    Add browserSync, error-handling, and file organisation to gulp code

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
+
+  gulp.task('watch',function(){
+
+    browserSync.init({
+      notify: false, // stops a wee notification appearing when the page is updated
+      server: {
+        baseDir: "app" // relative to where gulpfile.js is
+      }
+    })
+
+    watch('./app/index.html',function(){
+      browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css',function(){
+      gulp.start('cssInject');
+    });
+
+  });
+
+  gulp.task('cssInject', ['styles'],function(){
+    return gulp.src('./app/temp/styles/styles.css')
+          .pipe(browserSync.stream());
+  });
